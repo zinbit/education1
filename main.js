895f123bc3a055647b0392552b01d4bce068abee
Zinchenko Vitaliy <zin.bit@mail.ru>
2019-07-11 19:52:14 +0300

    адаптивность

--- main.js
+++ main.js
@@ -27,3 +27,41 @@ var d = hipitinuse(7, 72);
 var slides = document.getElementsByClassName('slide');
 console.log(slides[0]);
 slides[0].classList.add('active');
+
+var name = ('Максим');
+function func() {
+   // console.log (name)
+}
+setTimeout(func, 2000);
+
+
+var i = (0);
+function func() {
+   // console.log ((i) + 1)
+}
+setInterval (func, 2000);
+
+
+var i = (0);
+var timerId = setInterval(function() {
+    // console.log ( i = i + 1 );
+     }, 1000);
+
+     var activeSlide = 0;
+     var prevActiveSlide = 0;
+     
+     function showName() {
+       slides[prevActiveSlide].classList.remove('active');
+       slides[activeSlide].classList.add('active');
+     
+       prevActiveSlide = activeSlide;
+       activeSlide = activeSlide + 1;
+     
+       if (activeSlide === slides.length) {
+         activeSlide = 0;
+       }
+     };
+     
+     showName();
+     
+     setInterval(showName, 2000);
